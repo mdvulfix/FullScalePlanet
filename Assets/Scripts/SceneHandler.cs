aa2d6f0a1f3c12bdd96489515a128a2745ebe195
mdvulfix <54022658+mdvulfix@users.noreply.github.com>
2020-11-27 19:18:20 +0300

    Start 27/11/2020
    Start 27/11/2020

---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneHandler : MonoBehaviour
+{
+    
+    public string objectName = "Plane";
+
+
+    void Start()
+    {
+        
+        CreatePlane(objectName);
+
+
+
+    }
+
+
+    void CreatePlane(string name)
+    {
+        CreateObject(name);
+        CreateMesh();
+        CreateTexture();
+
+
+
+
+    }
+    
+    void CreateObject(string name)
+    {
+        GameObject plane = new GameObject(name);
+        
+    }
+
+
+    void CreateMesh()
+    {
+
+        CreateMeshFilter();
+        CreateMeshRenderer();
+    
+    }
+
+    void CreateMeshFilter()
+    {
+
+
+
+    }
+    void CreateMeshRenderer()
+    {
+
+
+    }
+
+
+    void CreateTexture()
+    {
+
+
+    }
+
+}
